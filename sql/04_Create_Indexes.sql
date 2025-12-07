c099328a37092cf651f7bd4f3dfc4d1793e0be1e
wullannnlbn <wulanlumbantoruan155@gmail.com>
2025-12-07 13:12:49 +0700

    update query

--- sql/04_Create_Indexes.sql
+++ sql/04_Create_Indexes.sql
@@ -1,17 +1,8 @@
 USE DM_BiroPerencanaan_DW;
 GO
 
--- 4.5.2 Step 2: Indexing Strategy
-
--- =========================================================
--- A. TABEL: FACT_ANGGARAN
--- =========================================================
-
--- 1. Clustered Index on Fact Table
--- Karena tabel sudah punya Primary Key (PK), PK tersebut sudah menjadi Clustered Index. 
-
--- 2. Non-Clustered Indexes
--- Index untuk foreign keys (join optimization)
+-- FACT_ANGGARAN
+-- Non-Clustered Indexes
 
 CREATE NONCLUSTERED INDEX IX_Fact_Anggaran_Unit
 ON dbo.Fact_Anggaran(unit_id)
@@ -28,13 +19,12 @@ ON dbo.Fact_Anggaran(kegiatan_id, waktu_key)
 INCLUDE (unit_id, pagu);
 GO
 
--- Covering index untuk common queries
 CREATE NONCLUSTERED INDEX IX_Fact_Anggaran_Covering
 ON dbo.Fact_Anggaran(waktu_key, unit_id)
 INCLUDE (program_id, pagu, realisasi, selisih_pagu_realisasi);
 GO
 
--- 3. Columnstore Index (for large fact tables)
+-- Columnstore Index (for large fact tables)
 CREATE NONCLUSTERED COLUMNSTORE INDEX NCCIX_Fact_Anggaran
 ON dbo.Fact_Anggaran
 (
@@ -50,15 +40,8 @@ ON dbo.Fact_Anggaran
 );
 GO
 
-
--- =========================================================
--- B. TABEL: FACT_KINERJA 
--- =========================================================
-
--- 1. Clustered Index on Fact Table
--- Karena tabel sudah punya Primary Key (PK), PK tersebut sudah menjadi Clustered Index. 
-
--- 2. Non-Clustered Indexes
+-- FACT_KINERJA 
+-- Non-Clustered Indexes
 CREATE NONCLUSTERED INDEX IX_Fact_Kinerja_Unit
 ON dbo.Fact_Kinerja(unit_id)
 INCLUDE (target, capaian_value, persentase_capaian);
@@ -69,13 +52,12 @@ ON dbo.Fact_Kinerja(indikator_id)
 INCLUDE (target, capaian_value);
 GO
 
--- Covering index
 CREATE NONCLUSTERED INDEX IX_Fact_Kinerja_Covering
 ON dbo.Fact_Kinerja(waktu_key, unit_id)
 INCLUDE (indikator_id, target, capaian_value);
 GO
 
--- 3. Columnstore Index
+-- Columnstore Index
 CREATE NONCLUSTERED COLUMNSTORE INDEX NCCIX_Fact_Kinerja
 ON dbo.Fact_Kinerja
 (
